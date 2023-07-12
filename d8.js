a01d1f781cc916a441a538fafe4343e4481ad4f9
serena guarnieri <s.guarnieri@hotmail.it>
2023-07-12 15:15:47 +0200

    d8

---
+++ d8.js
@@ -0,0 +1,23 @@
+document.querySelector("#push").onclick = function () {
+  if (document.querySelector("#newtask input").value.length == 0) {
+    alert("Inserisci il nome del tuo task");
+  } else {
+    document.querySelector("#tasks").innerHTML += `
+            <div class="task">
+                <span id="taskname">
+                    ${document.querySelector("#newtask input").value}
+                </span>
+                <button class="delete">
+                    <i class="far fa-trash-alt"></i>
+                </button>
+            </div>
+        `;
+
+    let current_tasks = document.querySelectorAll(".delete");
+    for (let i = 0; i < current_tasks.length; i++) {
+      current_tasks[i].onclick = function () {
+        this.parentNode.remove();
+      };
+    }
+  }
+};
